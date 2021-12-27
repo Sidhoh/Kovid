8459f08cf82ade96de4bdabf5fc89d8565078cde
roof <83639325+roooof@users.noreply.github.com>
2021-12-27 12:45:58 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Kovid',
+    version='0.1.0',
+    description = "A python lib to get info on covid-19 .",
+    author = "roof",
+    project_urls = {
+        "Github": "https://github.com/roooof/Kovid",
+    },
+    author_email = "danduhman7@gmail.com",
+    license = "MIT",
+    packages = find_packages(),
+    install_requires=[
+        'requests',
+        'json'
+    ]
+)
