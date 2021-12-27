8148df6bb5210303cc98522172c3b714976109be
roof <83639325+roooof@users.noreply.github.com>
2021-12-27 09:53:47 +0000

    updated kovid.py

--- kovid/Kovid.py
+++ kovid/Kovid.py
@@ -5,10 +5,10 @@ def jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
-def GetGlobalData():
+class GetGlobalData():
     globalData = requests.get("https://coronavirus-19-api.herokuapp.com/all")
     jprint(globalData.json())
 
-def GetCountryData():
+class GetCountryData():
     countryData = requests.get("https://coronavirus-19-api.herokuapp.com/countries")
     jprint(countryData.json())
