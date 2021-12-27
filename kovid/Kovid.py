4627585707e150eb5f0c1788261dc6782c3279c2
roof <83639325+roooof@users.noreply.github.com>
2021-12-27 19:25:00 +0530

    Update Kovid.py

--- kovid/Kovid.py
+++ kovid/Kovid.py
@@ -1,14 +1,18 @@
 import requests
 import json
 
-def jprint(obj):
+def __jprint(obj):
     text = json.dumps(obj, sort_keys=True, indent=4)
     print(text)
 
 def get_global_data():
     globalData = requests.get("https://coronavirus-19-api.herokuapp.com/all")
-    jprint(globalData.json())
+    __jprint(globalData.json())
 
 def get_countries_data():
     countries = requests.get("https://coronavirus-19-api.herokuapp.com/countries")
-    jprint(countries.json())
+    __jprint(countries.json())
+
+def get_country_data(country):
+    data = requests.get("https://coronavirus-19-api.herokuapp.com/countries/" + country)
+    __jprint(data.json())
