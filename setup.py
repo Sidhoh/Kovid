3c0b0d82701bc68e86d9bb8071f9db9ea46dabc2
roof <83639325+roooof@users.noreply.github.com>
2021-12-28 11:38:44 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Kovid',
-    version='0.1.0',
+    version='1.0.0',
     description = "A python lib to get info on covid-19 .",
     author = "roof",
     project_urls = {
